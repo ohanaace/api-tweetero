03c91f906ce5072529dd996d72c8700a772c803f
suelen <sreginaclsilva@gmail.com>
2023-04-09 20:52:57 -0300

    final commit

--- src/app.js
+++ src/app.js
@@ -9,37 +9,44 @@ const tweets = []
 const users = []
 
 app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body
-    if(!username || !avatar || typeof(username) !== "string" || typeof(avatar) !== "string"){
+    const { username, avatar } = req.body
+    if (!username || !avatar || typeof (username) !== "string" || typeof (avatar) !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
 
-    const user = {id: users.length + 1, username, avatar}
+    const user = { id: users.length + 1, username, avatar }
     users.push(user)
     res.status(201).send("OK")
 })
 
 app.post("/tweets", (req, res) => {
-const {username, tweet} = req.body
-const loggedUser = users.find((u) => u.username === username)
-if(!loggedUser){
-    return res.status(401).send("UNAUTHORIZED")
-}
-if(!tweet || typeof(tweet) !== "string" ){
-    return res.status(400).send("Todos os campos são obrigatórios!")
-}
-const newTweet = {id: tweets.length + 1, username, tweet}
-tweets.push(newTweet)
-res.status(201).send("OK")
+    const { username, tweet } = req.body
+    const loggedUser = users.find((u) => u.username === username)
+    if (!loggedUser) {
+        return res.status(401).send("UNAUTHORIZED")
+    }
+    if (!tweet || typeof (tweet) !== "string") {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+    const newTweet = { id: tweets.length + 1, username, tweet }
+    tweets.push(newTweet)
+    res.status(201).send("OK")
 })
 
 app.get("/tweets", (req, res) => {
-    if(tweets.length > 10){
-        const diff = tweets.length - 10
-        const recentTweets = tweets.filter((rec) => rec.id > diff)
+const twitter = tweets.map((twt) => {
+    const twitterUser = users.find((user) => user.username === twt.username)
+    const picture = twitterUser.avatar
+
+    return {...twt, avatar: picture}
+})
+    console.log(twitter)
+    if (twitter.length > 10) {
+        const diff = twitter.length - 10
+        const recentTweets = twitter.filter((rec) => rec.id > diff)
         return res.send(recentTweets)
     }
-    res.send(tweets)
+    res.send(twitter)
 })
 
 const PORT = 5000
